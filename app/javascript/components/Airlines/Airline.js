59ef5b18474525574939dd6f669e3b0dced32a97
zayneio <hello@zayne.io>
2020-02-25 16:27:03 -0500

    add styled components

--- app/javascript/components/Airlines/Airline.js
+++ app/javascript/components/Airlines/Airline.js
@@ -1,20 +1,54 @@
 import React from 'react'
-import { BrowserRouter as Router, Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const Card = styled.div`
+  border: 1px solid #efefef;
+  background: #fff;
+`
+
+const AirlineLogo = styled.div`
+  height: 50px;
+`
+
+const AirlineName = styled.div`
+  padding:20px;
+`
+
+const LinkWrapper = styled.div`
+  margin: 20px 0;
+  height:50px;
+
+  a {
+    color: #fff;
+    background-color: #71b406;
+    border-radius: 4px;
+    padding: 10px 30px;
+    cursor: pointer;
+    border-radius: 3px;
+    border: 1px solid #71b406;
+    text-align: center;
+    line-height: 20px;
+    min-height: 40px;
+    margin: 7px;
+    font-weight: 600;
+    text-decoration: none;
+  }
+`
 
 const Airline = (props) => {
   return (
-    <div className="card">
-      <div className="airline-logo">
-        <img src={props.image_url} alt={props.name} width="50" />
-      </div>
-      <div className="airline-name">
+    <Card>
+      <AirlineLogo>
+        <img src={props.image_url} alt={props.name} width="50"/>
+      </AirlineLogo>
+      <AirlineName>
         {props.name}
-      </div>
-      <div className="link-wrapper">
+      </AirlineName>
+      <LinkWrapper>
         <Link to={"/" + props.slug}>View Airline</Link>
-      </div>
-    </div>
+      </LinkWrapper>
+    </Card>
   )
 }
 
-export default Airline
+export default Airline 
--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,6 +1,38 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import Airline from './Airline'
+import styled from 'styled-components'
+
+const Home = styled.div`
+  text-align:center;
+`
+
+const Header = styled.div`
+  padding:100px 100px 10px 100px;
+  
+  h1 {
+    font-size:42px;
+  }
+`
+
+const Subheader = styled.p`
+  font-weight:300;
+  font-size:26px;
+`
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 20px;
+  width: 100%;
+  padding:20px;
+
+  > div {
+    background-color: #fff;
+    border-radius: 5px;
+    padding: 20px;
+  }
+`
 
 class Airlines extends Component {
   state = {
@@ -33,15 +65,15 @@ class Airlines extends Component {
     }
 
     return (
-      <div className="home">
-        <div className="header">
+      <Home>
+        <Header>
           <h1>OpenFlights</h1>
-          <p className="subheader">Honest, unbiased airline reviews. Share your experience.</p>
-        </div>
-        <div className="grid">
+          <Subheader>Honest, unbiased airline reviews. Share your experience.</Subheader>
+        </Header>
+        <Grid>
          {airlines}
-        </div>
-      </div>
+        </Grid>
+      </Home>
     )
   }
 }
