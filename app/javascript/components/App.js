65c70f2490e6673028a774ca5c833e9130bcd7bf
zayneio <hello@zayne.io>
2020-02-25 16:19:45 -0500

    add react-router-dom to our project and set up routing in react

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react'
+import { Route, Switch } from 'react-router-dom'
 
 class App extends Component {
   render(){
-    return (<div>Hello World</div>)
+    return(
+      <Switch>
+        <Route exact path="/" component="Airlines" />
+        <Route exact path="/:slug" component="Airline" />
+      </Switch>
+    )
   }
 }
 
--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import App from '../components/App'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <App />,
+    <Router>
+      <Route path="/" component="App"/>
+    </Router>,
     document.body.appendChild(document.createElement('div')),
   )
 })
