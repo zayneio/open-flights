90b7a51d41a40ada4ce4557a0cc3004c534a3192
zayneio <hello@zayne.io>
2020-02-25 16:03:14 -0500

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Airline.create([
+  { 
+    name: "United Airlines",
+    image_url: "https://open-flights.s3.amazonaws.com/United-Airlines.png"
+  }, 
+  { 
+    name: "Southwest",
+    image_url: "https://open-flights.s3.amazonaws.com/Southwest-Airlines.png"
+  },
+  { 
+    name: "Delta",
+    image_url: "https://open-flights.s3.amazonaws.com/Delta.png" 
+  }, 
+  { 
+    name: "Alaska Airlines",
+    image_url: "https://open-flights.s3.amazonaws.com/Alaska-Airlines.png" 
+  }, 
+  { 
+    name: "JetBlue",
+    image_url: "https://open-flights.s3.amazonaws.com/JetBlue.png" 
+  }, 
+  { 
+    name: "American Airlines",
+    image_url: "https://open-flights.s3.amazonaws.com/American-Airlines.png" 
+  }
+])
+      
