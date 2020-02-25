1e929e211041b38bc3a1ba5542d5146d53ef08c7
zayneio <hello@zayne.io>
2020-02-25 16:02:50 -0500

    add average score method to airline model

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -3,7 +3,16 @@ class Airline < ApplicationRecord
 
   before_create :slugify
 
+  # Slugify airline name into a url safe param before create
+  # Ex: 'United Airlines'.parameterize => 'united-airlines'
   def slugify
     self.slug = name.parameterize
   end
+
+  # Get the average score of all review for an airline
+  def avg_score
+    return 0 unless reviews.size.positive?
+
+    (reviews.sum(:score).to_f / reviews.count.to_f).to_f
+  end 
 end
