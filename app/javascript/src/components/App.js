1d7821d83084ae14e217e61a81537da64e03e9e1
zayneio <hello@zayne.io>
2020-03-04 17:21:02 -0500

    add authentication

--- app/javascript/src/components/App.js
+++ app/javascript/src/components/App.js
@@ -5,28 +5,14 @@ import Login from './Auth/Login'
 import Register from './Auth/Register'
 import Airlines from './Airlines/Airlines'
 import Airline from './Airline/Airline'
-import axios from 'axios'
 
 const App = (props) => {
-  const [currentUser, setCurrentUser] = useState(false)
-
-  useEffect( () => authenticate(), [])
-
-  const authenticate = () => {
-    axios.get('/api/v1/auth/logged_in', { withCredentials: true })
-    .then(resp => {
-      console.log('response', resp.data)
-      setCurrentUser(resp.data.logged_in)
-    })
-    .catch(resp => setCurrentUser(false) )
-  }
-
   return(
     <Switch>
-      <Route exact path="/" render={ props => <Airlines { ...props } currentUser={currentUser} /> } />
-      <Route exact path="/airlines/:slug" render={ props => <Airline { ...props } currentUser={currentUser} /> } />
-      <Route exact path="/login" render={ props => <Login { ...props } currentUser={currentUser} /> } />
-      <Route exact path="/register" render={ props => <Register { ...props } currentUser={currentUser} /> } />
+      <Route exact path="/" component={Airlines} />
+      <Route exact path="/airlines/:slug" component={Airline} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
     </Switch>
   )
 }
--- app/javascript/src/utils/Auth/Authenticate.js
+++ app/javascript/src/utils/Auth/Authenticate.js
@@ -4,12 +4,10 @@ const Authenticate = async () => {
   let loggedIn = false
 
   await axios.get('/api/v1/auth/logged_in', { withCredentials: true })
-  .then(resp => {
+  .then( resp => {
     loggedIn = resp.data.logged_in
-
-    return true
   })
-  .catch(resp => false )
+  .catch( err => console.log(err) )
 
   return loggedIn
 }
