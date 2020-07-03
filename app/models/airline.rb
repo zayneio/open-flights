a8057bc317b6c6870d34d7643c9f98325928c092
zayneio <hello@zayne.io>
2020-07-03 18:11:28 -0400

    Update dependencies, add validations, add specs

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,13 +1,15 @@
 class Airline < ApplicationRecord
   has_many :reviews
 
+  validates :name, presence: true, length: { maximum: 255 }
+
   # Slugify airline name into a url safe param before create
   # Ex: 'United Airlines'.parameterize => 'united-airlines'
   before_create -> (airline) do
     airline.slug = airline.name.parameterize
   end
 
-  # Get the average score of all review for an airline
+  # Get the average score of all reviews for an airline
   def calculate_average
     return 0 unless reviews.size.positive?
 
--- spec/models/airline_spec.rb
+++ spec/models/airline_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Airline, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#slugify' do
+    airline = Airline.create(name: 'Fake Airlines')
+
+    it { expect(airline.slug).to eq('fake-airlines')}
+  end
 end
